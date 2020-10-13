478e2b5b4855a4854de2107b33014ba100834eb6
Donnie Corbitt <70149211+kiiwiiwastaken@users.noreply.github.com>
2020-10-12 20:53:39 -0500

    Create furniture.lua
    Not useable yet, still updating the table.

---
+++ furniture.lua
@@ -0,0 +1,62 @@
+-- This is the furniture of Animal Crossing New Horizons. This is a LOOOOOOOOOOOOOOOOOOONG list of shit
+local furniture = {
+    -- A
+    ['Acoustic Guitar'] = 3120,
+    ['Angled Signpost'] = 600,
+    ['Apple Chair'] = 2480,
+    ['Aquarius Urn'] = 22125,
+    ['Aries Rocking Chair'] = 12125,
+    ['Asteroid'] = 4000,
+    ['Astronaut Suit'] = 6250,
+
+    -- B
+    ['Bamboo Basket'] = 1120,
+    ['Bamboo Bench'] = 1280,
+    ['Bamboo Doll'] = 2400,
+    ['Bamboo Floor Lamp'] = 1280,
+    ['Bamboo Noodle Slide'] = 3160,
+    ['Bamboo Partition'] = 2020, -- Shit year lol
+    ['Bamboo Shelf'] = 2400,
+    ['Bamboo Speaker'] = 1230,
+    ['Bamboo Stool'] = 800,
+    ['Bamboo Stopblock'] = 480,
+    ['Barbell'] = 7500,
+    ['Barrel'] = 2100,
+    ['Beekeeper\'s Hive'] = 2400,
+    ['Big Festive Tree'] = 3200,
+    ['Birdbath'] = 400,
+    ['Birdcage'] = 960,
+    ['Birdhouse'] = 840, -- in your soul!
+    ['Blossom-Viewing Lantern'] = 2880,
+    ['Bonfire'] = 1230,
+    ['Bonsai Shelf'] = 8460,
+    ['Brick Oven'] = 3820,
+    ['Brick Well'] = 2800,
+    ['Bunny Day Arch'] = 4800, -- fuck bunny day. Zipper is a piece of shit
+    ['Bunny Day Bed'] = 2400,
+    ['Bunny Day Festive Balloons'] = 1200,
+    ['Bunny Day Merry Balloons'] = 1200,
+    ['Bunny Day Stool'] = 1200,
+    ['Bunny Day Table'] = 1600,
+    ['Bunny Day Vanity'] = 1600,
+    ['Butter Churn'] = 1980,
+
+    -- C
+    ['Campfire'] = 30, -- 30 fucking bells????
+    ['Cancer Table'] = 21975,
+    ['Capricorn Ornament'] = 22650,
+    ['Cardboard Bed'] = 240,
+    ['Cardboard Chair'] = 60,
+    ['Cardboard Sofa'] = 120,
+    ['Cardboard Table'] = 240,
+    ['Cherry-Blossom Branches'] = 4240,
+    ['Cherry-Blossom-Petal Pile'] = 2000,
+    ['Cherry-Blossom Pond Stone'] = 2700,
+    ['Clackercart'] = 960,
+    ['Clothesline'] = 100,
+    ['Cosmos Shower'] = 3050,
+    ['Crescent-Moon Chair'] = 8500,
+    ['Crewed Spaceship'] = 20000,
+
+    -- D (fun fact, this is 2kb so far, and we only have 3 letters done.)
+}
